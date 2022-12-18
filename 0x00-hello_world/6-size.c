4087d444b867867cc7f8b4dea451c2e2c3065f6b
Taviey <luvoctavia@yahoo.com>
2022-12-17 17:26:34 -0800

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,6 @@ int main(void)
 	printf("size of int: %i byte(s)\n", sizeof(int));
 	printf("size of long int: %i byte(s)\n", sizeof(long int));
 	printf("size of float: %i byte(s)\n"' sizeof(float));
+	printf("size of long long int: %i byte(s)\n", sizeof(long long int));
 	return (0);
 }
